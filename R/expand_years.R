4cec5782d714b6906a2229ce9d2fb35060823d8d
David Holstius <david.holstius@gmail.com>
2019-09-16 14:32:21 -0700

    handle case when `year` is missing

--- R/expand_years.R
+++ R/expand_years.R
@@ -1,21 +1,31 @@
 #' @export
 expand_years <- function (
   input_data,
-  years
+  years,
+  year_var = "year",
+  verbose = getOption("verbose")
 ) {
 
+  msg <- function (...) if(isTRUE(verbose)) message("[expand_years] ", ...)
+
   f <- function (...) return(years)
+
+  if (year_var %not_in% names(input_data)) {
+    warn_msg <- paste0("`", year_var, "` isn't in your original data; adding it")
+    msg(warn_msg)
+    input_data$year <- NA_integer_
+  }
 
   nested <-
     dplyr::mutate_at(
       input_data,
-      vars(year),
+      vars(year_var),
       ~ purrr::map(., f))
 
   expanded <-
     tidyr::unnest(
       nested,
-      cols = c(year))
+      cols = c(year_var))
 
   return(expanded)
 
