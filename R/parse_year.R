b1a65b9d83d64cc9adbf6a559144cfae13660e0c
David Holstius <david.holstius@gmail.com>
2019-09-19 15:04:22 -0700

    S3 upgrade and improved tests for parse_year()

--- R/parse_year.R
+++ R/parse_year.R
@@ -1,17 +1,53 @@
+#' parse_year
+#'
+#' Extracts a four-digit "naive" representation from various other representations of a "year".
+#'
+#' @param x character, numeric, or YYYY-class
+#' @param ... ignored for now
+#'
+#' @return four-digit integer
+#'
+#' @examples
+#' parse_year(CY(2030))
+#' parse_year(RY(2015))
+#' parse_year(BY(2011))
+#' parse_year("1999")
+#'
 #' @export
-parse_year <- function (x) {
+parse_year <- function (x, ...) {
+  UseMethod("parse_year", x)
+}
 
-  if (is.character(x)) {
-    yyyy <- parse_number(x)
-  } else if (inherits(x, "YYYY")) {
-    yyyy <- as.integer(x)
-  } else if (hasMethod("as.integer", class(x))) {
-    yyyy <- as.integer(x)
-  } else {
-    msg <- str_c("[parse_year] Don't know how to parse ", as.character(x))
-    stop(msg)
+#' @method parse_year character
+#' @export
+parse_year.character <- function (x, ...) {
+  parsed <- as.integer(readr::parse_number(x))
+  is_four_digits <- (parsed >= 1000) && (parsed <= 9999)
+  if (!all_true(is_four_digits)) {
+    err_msg <- stringr::str_c(
+      "some parsed values are not four digits")
+    stop(err_msg)
+  }
+  return(parsed)
+}
+
+#' @method parse_year YYYY
+#' @export
+parse_year.YYYY <- function (x, ...) {
+  parsed <- parse_year.character(as.character(x))
+  return(parsed)
+}
+
+#' @method parse_year default
+#' @export
+parse_year.default <- function (x, ...) {
+
+  if (hasMethod("as.character", class(x))) {
+    parsed <- parse_year.character(as.character(x))
+    return(parsed)
   }
 
-  return(yyyy)
+  err_msg <- str_c("[parse_year] Don't know how to parse an object of class ", class(x))
+  stop(err_msg)
 
 }
