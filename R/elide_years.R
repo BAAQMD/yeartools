99fce81701d442721e7426a15b3cdc510ade182c
David Holstius <david.holstius@gmail.com>
2019-10-17 10:52:56 -0700

    add S3 generic elide_years() and associated tests

---
+++ R/elide_years.R
@@ -0,0 +1,41 @@
+#' elide_years
+#'
+#' To elide distinctions between "CY", "BY", "RY", etc.
+#'
+#' @export
+elide_years <- function (x, ...) {
+  UseMethod("elide_years")
+}
+
+#' elide_years
+#'
+#' @method elide_years default
+#' @export
+elide_years.default <- function (x, ...) {
+  elide_year(x, ...)
+}
+
+#' elide_years
+#'
+#' @method elide_years data.frame
+#' @export
+elide_years.data.frame <- function (x, ...) {
+
+  if ("year" %in% names(x)) {
+
+    result <-
+      dplyr::mutate_at(
+        x,
+        vars(year),
+        ~ elide_year(., ...))
+
+  } else {
+
+    # FIXME: issue a soft warning?
+    result <- x
+
+  }
+
+  return(result)
+
+}
