a4130e31d88ff660cb2aac605821e1dd4338065c
David Holstius <david.holstius@gmail.com>
2021-03-03 09:31:37 -0800

    fix comparability tests

--- tests/testthat/test-YYYY-compare.R
+++ tests/testthat/test-YYYY-compare.R
@@ -1,27 +1,26 @@
-test_that("equality works", {
-
+test_that("basic equality/inequality works", {
   expect_true(CY(2011) == CY(2011))
+  expect_true(CY(2011) != CY(2012))
+})
+
+test_that("four-digit strings are comparable", {
+  expect_false(CY(2011) == "CY2012")
   expect_true(CY(2011) == "CY2011")
   expect_true("CY2011" == CY(2011))
-
   expect_equal(
     CY(2011:2015) == paste0("CY", 2011:2015),
     c(TRUE, TRUE, TRUE, TRUE, TRUE))
+})
 
-  expect_false(CY(2011) == 2011)
-  expect_false(CY(2011) == "CY2012")
-
-  expect_true(CY(2011) != CY(2012))
-
+test_that("four-digit numbers are not comparable", {
+  expect_error(CY(2011) == 2011)
 })
 
 test_that("different timelines are not comparable", {
-
-  expect_false(RY(2011) == PY(2011))
-
+  expect_error(RY(2011) == PY(2011))
 })
 
-test_that("ordering works", {
+test_that("basic ordering works", {
 
   expect_true(CY(2011) < CY(2012))
   expect_true(CY(2011) < "CY2012")
