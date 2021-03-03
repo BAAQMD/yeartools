185961d9a6c896dca747a6cc256bbf6a2e356052
David Holstius <david.holstius@gmail.com>
2021-03-03 09:37:00 -0800

    fix S3 methods and documentation for elide_year()

--- R/elide_year.R
+++ R/elide_year.R
@@ -6,35 +6,16 @@
 #' @param x `RY`, `PY`, `CY`, or `BY` object
 #' @param verbose logical
 #'
+#' @note `elide_years()` is a backwards-compatible alias for `elide_year()`.
 #' @aliases elide_years
 #'
 #' @importFrom stringr str_match
-#'
-#' @export
-elide_year <- function (
-  x,
-  verbose = getOption("verbose")
-) {
-  pattern <- "^([CRPB]Y)?([0-9]{4})$"
-  matches <- stringr::str_match(as.character(x), pattern = pattern)
-  year <- as.integer(matches[, ncol(matches)])
-  return(year)
-}
-
-#' elide_years
-#'
-#' To elide distinctions between "CY", "BY", "RY", etc.
-#'
-#' @param x `CY`, `BY`, `RY`, or `PY` object
-#' @param ... futher arguments
 #'
 #' @export
 elide_year <- function (x, ...) {
   UseMethod("elide_year")
 }
 
-#' @rdname elide_years
-#' @method elide_years default
 #' @export
 elide_year.default <- function (
   x,
@@ -46,8 +27,6 @@ elide_year.default <- function (
   return(elided)
 }
 
-#' @rdname elide_years
-#' @method elide_years data.frame
 #' @export
 elide_year.data.frame <- function (x, ...) {
   if ("year" %in% names(x)) {
@@ -59,7 +38,10 @@ elide_year.data.frame <- function (x, ...) {
   return(result)
 }
 
-#' @note `elide_years()` is a backwards-compatible alias for `elide_year()`.
-#' @describeIn elide_year
-#' @export
+#' elide_years
+#' @noRd
+elide_years <- elide_year
+
+#' elide_years
+#' @noRd
 elide_years <- elide_year
