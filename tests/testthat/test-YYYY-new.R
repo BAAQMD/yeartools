eddd6e2c2a9f5237ffd096e4c28964f392a86af0
David Holstius <david.holstius@gmail.com>
2021-03-03 09:32:40 -0800

    fix accessor expectations within constructor tests

--- tests/testthat/test-YYYY-new.R
+++ tests/testthat/test-YYYY-new.R
@@ -2,23 +2,24 @@ test_that("YYYY() works", {
 
   # single year
   x <- YYYY(2005, prefix = "CY")
-  expect_equal(year(x), 2005)
-  expect_equal(timeline(x), "CY")
+  expect_identical(year(x), "2005")
+  expect_identical(parse_year(x), 2005L)
+  expect_identical(timeline(x), "CY")
 
   # sequence of years
   x <- YYYY(2000:2010, prefix = "CY")
-  expect_equal(year(x), 2000:2010)
-  expect_equal(timeline(x), "CY")
+  expect_identical(parse_year(x), 2000:2010L)
+  expect_identical(timeline(x), "CY")
 
   # unsorted vector of years
   x <- YYYY(c(2005, 2010, 2008), prefix = "CY")
-  expect_equal(year(x), c(2005, 2010, 2008))
-  expect_equal(timeline(x), "CY")
+  expect_identical(parse_year(x), c(2005L, 2010L, 2008L))
+  expect_identical(timeline(x), "CY")
 
   # dots
   x <- YYYY(2003, 2004:2009, prefix = "CY")
-  expect_equal(year(x), c(2003, 2004:2009))
-  expect_equal(timeline(x), "CY")
+  expect_identical(parse_year(x), c(2003L, 2004:2009L))
+  expect_identical(timeline(x), "CY")
 
   # character (all with same prefix)
   x <- YYYY(c("CY1990", "CY2000"))
@@ -50,7 +51,8 @@ test_that("PY(2011:2012) works", {
   x <- PY(2011:2012)
   expect_identical(timeline(x), "PY")
   expect_equal(length(x), 2)
-  expect_identical(elide_year(x), c(2011L, 2012L))
+  expect_identical(parse_year(x), c(2011L, 2012L))
+  expect_identical(elide_year(x), c("2011", "2012"))
 })
 
 test_that("can't have two different prefixes", {
