a8dd2aa9b1e8cfd9963059254a256fc0a7dc123f
David Holstius <david.holstius@gmail.com>
2021-03-18 12:52:10 -0700

    consolidate timeline() code

--- R/timeline.R
+++ R/timeline.R
@@ -1,7 +1,21 @@
-timeline <- function (
-  x,
-  pattern = "^([CRPB]Y)?([0-9]{4})$"
-) {
+#' timeline
+#'
+#' Get the timeline ("RY", "CY", etc.) associated with a `YYYY` object.
+#'
+#' @param x [YYYY] object
+#' @param pattern `x` needs to match this
+#'
+#' @export
+timeline <- function (x) {
+  UseMethod("timeline")
+}
+
+#' @rdname timeline
+#'
+#' @export
+timeline.YYYY <- function (x) {
+
+  pattern <- "^([CRPB]Y)?([0-9]{4})$"
 
   if (isFALSE(is.null(attr(x, "timeline")))) {
     return(attr(x, "timeline"))
